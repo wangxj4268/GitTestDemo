6152fa20faab6808a1b8260e154325475bb9ad20
wangxj4268 <2540942304@qq.com>
2019-04-03 10:41:24 +0800

    提交
    初始化

---
+++ GitTestDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitTestDemo
+//
+//  Created by 王雪剑 on 2019/4/3.
+//  Copyright © 2019年 zkml－wxj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitTestDemo/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  GitTestDemo
+//
+//  Created by 王雪剑 on 2019/4/3.
+//  Copyright © 2019年 zkml－wxj. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
